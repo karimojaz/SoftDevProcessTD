e106c25974ae9b522e6f098ad02efe5dc7d6fd48
Victor Querette <vquerette@gmail.com>
2017-09-22 19:03:52 +0200

    Initial commit

---
+++ app/src/main/java/com/teamTitan/app/App.java
@@ -0,0 +1,13 @@
+package com.teamTitan.app;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class App 
+{
+    private static final Logger LOGGER = Logger.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        BasicConfigurator.configure();
+        LOGGER.error( "Hello World!" );
+    }
+}
