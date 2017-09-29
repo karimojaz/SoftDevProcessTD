dfb4dac4c9fe2bd8e99c4199eebbaecab598d8cd
Victor Querette <vquerette@gmail.com>
2017-09-29 16:58:09 +0200

    Commit

---
+++ src/main/java/TITAN/App.java
@@ -0,0 +1,13 @@
+package TITAN;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- src/test/java/TITAN/AppTest.java
+++ src/test/java/TITAN/AppTest.java
@@ -1,4 +1,4 @@
-package com.teamTitan.app;
+package TITAN;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
