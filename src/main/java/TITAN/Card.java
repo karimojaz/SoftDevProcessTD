eb8ec38fd8c27125f1ef25b2bd66722554b41db2
Victor Querette <vquerette@gmail.com>
2017-11-03 18:08:57 +0100

    Victor

--- src/main/java/TITAN/Card.java
+++ src/main/java/TITAN/Card.java
@@ -1,10 +1,6 @@
 package TITAN;
 
-import java.awt.*;
-
 public abstract class Card {
-
-    protected Image image;
-
+    
     abstract void activate();
 }
---
+++ src/main/java/TITAN/Deck.java
@@ -0,0 +1,4 @@
+package TITAN;
+
+public class Deck {
+}
