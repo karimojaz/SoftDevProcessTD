403c4b1bc01cbd7c2e4c2ba3cd0774ab48cb7e7e
Victor Querette <vquerette@gmail.com>
2017-11-03 18:38:02 +0100

    Victor

--- src/main/java/TITAN/Deck.java
+++ src/main/java/TITAN/Deck.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 
 public class Deck
 {
-    ArrayList<Card> cards;
+    private ArrayList<Card> cards;
 
+    public Deck(int size)
+    {
+        cards = new ArrayList<Card>(size);
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
 }
