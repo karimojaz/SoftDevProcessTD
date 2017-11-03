24a0df9dd748d834a23de737175b57424ac996c2
Victor Querette <vquerette@gmail.com>
2017-11-03 18:54:52 +0100

    Victor

--- src/main/java/TITAN/Kingdom.java
+++ src/main/java/TITAN/Kingdom.java
@@ -1,4 +1,21 @@
 package TITAN;
 
-public class Kingdom {
+import java.util.ArrayList;
+
+public class Kingdom
+{
+    private ArrayList<Card> cardsInKingdom;
+    private Player owner;
+
+    public Kingdom(Player owner)
+    {
+        this.owner = owner;
+        cardsInKingdom = new ArrayList<Card>();
+    }
+
+    public Player getOwner() { return owner; }
+
+    public ArrayList<Card> getCardsInKingdom() {
+        return cardsInKingdom;
+    }
 }
