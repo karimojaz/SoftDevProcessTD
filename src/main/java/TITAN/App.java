9d4f6084e0960ae828b296f7ca918c6b9e9d861c
Victor Querette <vquerette@gmail.com>
2017-11-03 17:38:50 +0100

    jj

--- src/main/java/TITAN/App.java
+++ src/main/java/TITAN/App.java
@@ -2,9 +2,7 @@ package TITAN;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println("Hello World!");
     }
-    aze
 }
