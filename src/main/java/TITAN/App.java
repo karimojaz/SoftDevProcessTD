3cec1849f6bb34b036f19c8b383c69c57e6cd0a2
brissax <sabrilachihab@gmail.com>
2017-11-17 19:02:41 +0100

    AJout d'une interfae graphique fontionnelle au démarrage

--- src/main/java/TITAN/App.java
+++ src/main/java/TITAN/App.java
@@ -1,10 +1,28 @@
 package TITAN;
 
 
-public class App {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    /*public static void main(String[] args) {
 
         System.out.println("Hello World!");
 
+    }*/
+
+    private Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.stage=primaryStage;
+        stage.setTitle("TITAN GAME");
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/TitanGame.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
