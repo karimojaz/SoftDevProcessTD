907d2d176289acd6c0a3b834cd1cbe487fb338e7
Victor Querette <vquerette@gmail.com>
2017-11-20 16:41:23 +0100

    Victor

--- src/main/java/TITAN/App.java
+++ src/main/java/TITAN/App.java
@@ -8,12 +8,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    /*public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-
-    }*/
-
     private Stage stage;
 
     @Override
