39b1c4858ec7dab8bdcabb217febfde4ef304854
kimberlyho <kimberly.ho@devinci.fr>
2017-11-23 10:23:32 +0100

    Kimberly

--- src/test/java/TITAN/ElfSteps.java
+++ src/test/java/TITAN/ElfSteps.java
@@ -12,14 +12,14 @@ public class ElfSteps {
 
     @Before public void setup() {playground = new Board();}
 
-    @When("^Alice play an elf$")
+    @When("^Alice plays an elf$")
         public void alice_plays_an_elf() throws  Throwable {
             playground.getActivePlayer().getHand().getCardsInHand().clear();
             playground.getActivePlayer().getHand().add(new Elf());
             playground.getActivePlayer().playCard(0);
         }
 
-    @Then("^Alice choose one card in the front of you$ and copy the effect if this")
+    @Then("^Alice choose one card in the front of you and Alice copy the effect of this card$")
         public void AliceChooseacard()
         {
             Card choose  = playground.getActivePlayer().getHand().chooseCard(1);
@@ -27,8 +27,8 @@ public class ElfSteps {
             playground.getActivePlayer().getHand().popAt(0);
         }
 
-    @Given("^There is only one card left in the left$")
-    public void There_is_only_one_card_left_in_the_left() throws Throwable {
+    @Given("^There is only one card left in the deck$")
+    public void There_is_only_one_card_left_in_the_deck() throws Throwable {
         playground.getDeck().getCards().clear();
     }
 
--- src/test/resources/elf.feature
+++ src/test/resources/elf.feature
@@ -4,10 +4,10 @@
 Feature: Elf managment effect
     
     Scenario: an elf is summoned
-          When Alice play an elf
+          When Alice plays an elf
           Then Alice choose one card in the front of you and Alice copy the effect of this card
 
     Scenario: an elf is played but there only one card left in the deck
-              Given There is only one card left in the left
-              When Alice plays a Elfe
-              Then the card go automaticly to the graveyard
+              Given There is only one card left in the deck
+              When Alice plays an elf
+              Then the card goes automatically to the kingdom
