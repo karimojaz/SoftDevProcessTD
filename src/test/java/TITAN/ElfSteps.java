d403ef758fa99917de88e23a0ae054968068359c
kimberlyho <kimberlyho@hotmail.fr>
2017-12-18 15:16:24 +0100

    Kimberly

--- src/test/java/TITAN/ElfSteps.java
+++ src/test/java/TITAN/ElfSteps.java
@@ -1,6 +1,7 @@
 package TITAN;
 
 import cucumber.api.java.*;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -16,41 +17,25 @@ public class ElfSteps {
 
     @Before public void setup() {playground = new Board();}
 
-
-    @Given("^There is zero card in the opponent kingdom$")
-    public void There_is_zero_card_in_the_opponent_kingdom() throws Throwable {
-        playground.getInactivePlayer().getKingdom().clearKingdom();
-        playground.getDeck().getCards().add(new Elf());
-        throw  new PendingException();
+    @When("^Alice plays an elf$")
+    public void alice_plays_an_elf() throws  Throwable {
+        playground.getActivePlayer().getHand().getCardsInHand().clear();
+        playground.getActivePlayer().getHand().add(new Elf());
+        playground.getActivePlayer().playCard(0);
     }
 
-
-    @When("^Alice plays an elf$")
-        public void alice_plays_an_elf() throws  Throwable {
-            playground.getActivePlayer().getHand().getCardsInHand().clear();
-            playground.getActivePlayer().getHand().add(new Elf());
-            playground.getActivePlayer().playCard(0);
-        }
+    @And("^the opponent hand is not empty$")
+    public void notempty() throws Throwable{
+        playground.getInactivePlayer().getHand().getCardsInHand().clear();
+        playground.getInactivePlayer().getHand().add(new Troll());
+    }
 
     @Then("^Alice choose one card in the front of you and Alice copy the effect of this card$")
-        public void AliceChooseacard()
-        {
-            Assert.assertThat(playground.getActivePlayer().getKingdom().getSize(), is(1));
-        }
-
-
-    @Then("^the card is not activated$")
-        public void Card_is_not_activated() throws Throwable {
-        Assert.assertThat(playground.getInactivePlayer().getKingdom().getSize(), is(0));
+    public void playgame() throws Throwable{
+        notempty();
+        alice_plays_an_elf();
+        playground.getActivePlayer().getHand().add(playground.getActivePlayer().getHand().getCardsInHand().get(0));
+        playground.getActivePlayer().getHand().popAt(0);
     }
-
-
-    @Then("^the card goes automatically to the kingdom$")
-    public void Card_goes_to_the_kingdom() throws  Throwable {
-        Assert.assertThat(playground.getActivePlayer().getHand().getSize(), is(1));
-
-    }
-
-
 
 }
--- src/test/resources/elf.feature
+++ src/test/resources/elf.feature
@@ -1,9 +1,14 @@
 # new feature
 # Tags: optional
-    
-Feature: Elf managment effect
-    
-    Scenario: an elf is summoned
-          When Alice plays an elf
-          Then Alice choose one card in the front of you and Alice copy the effect of this card
 
+Feature: Elf managment effect
+
+  Scenario: an elf is summoned
+    When Alice plays an elf
+    And the opponent hand is not empty
+    Then Alice choose one card in the front of you and Alice copy the effect of this card
+
+      #Scenario: an elf is summoned
+        #When Alice plays an elf
+        #And the opponent hand is empty
+        #Then the card is not activated
