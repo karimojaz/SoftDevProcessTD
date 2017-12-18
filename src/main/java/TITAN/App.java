00a358b5ef27613952b6e058904e5ddd24082bff
carel <elimbicarel@gmail.com>
2017-12-18 17:30:06 +0100

    fxml

--- src/main/java/TITAN/App.java
+++ src/main/java/TITAN/App.java
@@ -7,21 +7,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.ImageView;
+
 public class
 App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        /*primaryStage.setTitle("TITAN GAME");
-        Parent root = FXMLLoader.load(App.class.getResource("/fxml/Game.fxml"));
+        primaryStage.setTitle("TITAN GAME");
+        Parent root = FXMLLoader.load(App.class.getResource("/fxml/WelcomPage.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.sizeToScene();
-        primaryStage.show();*/
+        primaryStage.show();
 
-        Game g = new Game();
-        g.play();
+        //Game g = new Game();
+       // g.play();
     }
 }
